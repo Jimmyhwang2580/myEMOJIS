faf9eed33d0885b18847c0b1b1a4eaf458443536
Jimmyhwang2580 <80722675+Jimmyhwang2580@users.noreply.github.com>
2023-04-05 13:48:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const emojiContainer = document.getElementById("emoji-container")
+const pushBtn = document.getElementById("push-btn")
+const emojiInput = document.getElementById("emoji-input")
+const unshiftBtn = document.getElementById("unshift-btn")
+const popBtn = document.getElementById("pop-btn")
+const shiftBtn = document.getElementById("shift-btn")
 const myEmojis = ["👨‍💻", "👨‍👩‍👧‍👦", "🇰🇷", "🌯"]
 
 function renderEmojis() {
-    const emojiContainer = document.getElementById("emoji-container")
+    
     emojiContainer.innerHTML = ""
     for (let i = 0; i < myEmojis.length; i++) {
         const emoji = document.createElement('span')
@@ -12,9 +18,9 @@ function renderEmojis() {
 
 renderEmojis()
 
-const pushBtn = document.getElementById("push-btn")
+
 pushBtn.addEventListener("click", function(){
-    const emojiInput = document.getElementById("emoji-input")
+    
     if (emojiInput.value) {
         myEmojis.push(emojiInput.value)
         emojiInput.value = ""
@@ -22,9 +28,9 @@ pushBtn.addEventListener("click", function(){
     }
 })
 
-const unshiftBtn = document.getElementById("unshift-btn")
+
 unshiftBtn.addEventListener("click", function(){
-    const emojiInput = document.getElementById("emoji-input")
+    
     if (emojiInput.value) {
         myEmojis.unshift(emojiInput.value)
         emojiInput.value = ""
@@ -32,13 +38,13 @@ unshiftBtn.addEventListener("click", function(){
     }
 })
 
-const popBtn = document.getElementById("pop-btn")
+
 popBtn.addEventListener("click", function(){
     myEmojis.pop()
     renderEmojis()
 })
 
-const shiftBtn = document.getElementById("shift-btn")
+
 shiftBtn.addEventListener("click", function(){
     myEmojis.shift()
     renderEmojis()
